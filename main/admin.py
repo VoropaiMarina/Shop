a6f99dfc854ac485b73a799ca5e20cb872308025
marina <marina.voropai@gmail.com>
2021-12-15 08:14:55 +0300

    added class SmartphoneAdminForm

--- main/admin.py
+++ main/admin.py
@@ -8,6 +8,22 @@ from django import forms
 from .models import *
 
 
+class SmartphoneAdminForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        instance = kwargs.get('instance')
+        if not instance.sd:
+            self.fields['sd_volume_max'].widget.attrs.update({
+                'readonly': True, 'style': 'background: ligthgrey'
+            })
+
+    def clean(self):
+        if not self.cleaned_data['sd']:
+            self.cleaned_data['sd_volume_max'] = None
+        return self.cleaned_data
+
+
 class NotebookAdminForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
@@ -31,6 +47,9 @@ class NotebookAdmin(admin.ModelAdmin):
 
 
 class SmartphoneAdmin(admin.ModelAdmin):
+
+    change_form_template = 'admin.html'
+    form = SmartphoneAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
