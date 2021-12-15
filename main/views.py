58757104f3795de676a740cc684fef4f96c24a0a
marina <marina.voropai@gmail.com>
2021-12-15 19:55:12 +0300

    added get_models_for_count

--- main/views.py
+++ main/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 
-from .models import Notebook, Smartphone
+from .models import Notebook, Smartphone, Category
 
 
 def test_view(request):
-    return render(request, 'base.html', {})
+    categories = Category.objects.get_categories_for_left_sidebar()
+    return render(request, 'base.html', {'categories': categories})
 
 
 class ProductDetailView(DetailView):
@@ -19,6 +20,7 @@ class ProductDetailView(DetailView):
         self.model = self.CT_MODEL_MODEL_CLASS[kwargs['ct_model']]
         self.queryset = self.model._base_manager.all()
         return super().dispatch(request, *args, **kwargs)
+
     context_object_name = 'product'
     template_name = 'product_detail.html'
     slug_url_kwarg = 'slug'
