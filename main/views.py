20dbc07574b48b7ab3c6a987f2fb818fd279de1c
marina <marina.voropai@gmail.com>
2021-12-21 13:35:32 +0300

    added CartView

--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseRedirect
 from django.views.generic import DetailView, View
 
-from .models import Notebook, Smartphone, Category, LatestProducts, Customer, Cart
+from .models import Notebook, Smartphone, Category, LatestProducts, Customer, Cart, CartProduct
 from .mixins import CategoryDetailMixin
 
 
@@ -35,6 +37,11 @@ class ProductDetailView(CategoryDetailMixin, DetailView):
     template_name = 'product_detail.html'
     slug_url_kwarg = 'slug'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ct_model'] = self.model._meta.model_name
+        return context
+
 
 class CategoryDetailView(CategoryDetailMixin, DetailView):
 
@@ -43,6 +50,24 @@ class CategoryDetailView(CategoryDetailMixin, DetailView):
     context_object_name = 'category'
     template_name = 'category_detail.html'
     slug_url_kwarg = 'slug'
+
+
+class AddToCartView(View):
+
+    def get(self, request, *args, **kwargs):
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('product_slug')
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer, in_order=False)
+        content_type = ContentType.objects.get(model=ct_model)
+        product = content_type.model_class().objects.get(slug=product_slug)
+        cart_product = CartProduct.objects.create(
+            user=cart.owner,
+            cart=cart,
+            content_objects=product,
+            final_price=product.price
+        )
+        cart.products.add(cart_product)
+        return HttpResponseRedirect('/cart/')
 
 
 class CartView(View):
