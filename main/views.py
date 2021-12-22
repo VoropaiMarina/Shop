05cc96f1a6d2f06b162c9f4b77a6033bd43995a8
marina <marina.voropai@gmail.com>
2021-12-22 15:25:48 +0300

    added count in cart

--- main/views.py
+++ main/views.py
@@ -10,13 +10,16 @@ from .mixins import CategoryDetailMixin
 class BaseView(View):
 
     def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer)
         categories = Category.objects.get_categories_for_menu()
         products = LatestProducts.objects.get_products_for_main_page(
             'notebook', 'smartphone', with_respect_to='notebook'
         )
         context = {
             'categories': categories,
-            'products': products
+            'products': products,
+            'cart': cart
         }
         return render(request, 'base.html', context)
 
@@ -55,16 +58,13 @@ class CategoryDetailView(CategoryDetailMixin, DetailView):
 class AddToCartView(View):
 
     def get(self, request, *args, **kwargs):
-        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('product_slug')
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')
         customer = Customer.objects.get(user=request.user)
         cart = Cart.objects.get(owner=customer, in_order=False)
         content_type = ContentType.objects.get(model=ct_model)
         product = content_type.model_class().objects.get(slug=product_slug)
-        cart_product = CartProduct.objects.create(
-            user=cart.owner,
-            cart=cart,
-            content_objects=product,
-            final_price=product.price
+        cart_product, created = CartProduct.objects.get_or_create(
+            user=cart.owner, cart=cart, content_type=content_type, object_id=product.id, final_price=product.price
         )
         cart.products.add(cart_product)
         return HttpResponseRedirect('/cart/')
